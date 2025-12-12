9a6de3576662d4fa1e9e8d3a5ad544bc619dc889
Nurs <kasyrevevgeni@gmail.com>
2025-12-12 21:02:58 +0500

    test changes

--- main.py
+++ main.py
@@ -13,8 +13,3 @@ external_data = {
     "signup_ts": "2017-06-01 12:22",
     "friends": [1, "2", b"3"],
 }
-user = User(**external_data)
-print(user)
-# > User id=123 name='John Doe' signup_ts=datetime.datetime(2017, 6, 1, 12, 22) friends=[1, 2, 3]
-print(user.id)
-# > 123
